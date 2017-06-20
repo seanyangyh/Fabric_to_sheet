31125471d37bbdd3629b6b75c8e593ebbe5c7e89
楊喻翔 <seanyang@yangyuxiangs-MacBook-Pro.local>
2017-06-20 17:16:27 +0800

    Fix All Version crash rate always show 100%

--- Fill_in_sheet.py
+++ Fill_in_sheet.py
@@ -68,7 +68,7 @@ def fabric_crash_rate_uploader(data, date, spreadsheet_id, sheet_range, service)
     # clear data of Summary!A2:E first
     request = service.spreadsheets().values().clear(spreadsheetId=spreadsheet_id, range=sheet_range, body={}).execute()
     print(request)
-    for i in range(0, len(User_Input.Version), 1):
+    for i in range(0, len(User_Input.Version)+1, 1):
         crash_uv = data[User_Input.Version[i]]['CRASH-FREE USERS']
         crash_pv = data[User_Input.Version[i]]['CRASH-FREE SESSIONS']
         append_sheet = sheet_summary_append_handler(date, User_Input.Version[i], crash_uv, crash_pv, spreadsheet_id, sheet_range, service)

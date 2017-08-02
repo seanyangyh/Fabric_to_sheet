6be7c1748ab2bbbf766a661e74705eabebef4b6e
楊喻翔 <seanyang@yangyuxiangs-MacBook-Pro.local>
2017-08-02 15:45:59 +0800

    [Bugfix] replace crash count from k to 000.

--- Fill_in_sheet.py
+++ Fill_in_sheet.py
@@ -189,8 +189,7 @@ def history_occurrences_catcher(RecentActivity, crash_rate_data):
     temp_list_count = temp_list_count[:-2]
     crash_count_list = temp_list_count.split(', ')
     for i in range(0, len(crash_count_list), 1):
-        if crash_count_list[i].find("k") != -1:
-            crash_count_list[i] = crash_count_list[i][:-1] + '000'
+        crash_count_list[i] = crash_count_list[i].replace('k', '000')
 
     print(crash_count_list)
     dau_list = []
@@ -226,7 +225,7 @@ def history_occurrences_catcher(RecentActivity, crash_rate_data):
 def fabric_crashlytics_slope_criteria_uploader(tf_today, today, duplicate_list, crash_rate_data, data, spreadsheet_id, sheet_range, service):
     first_time_count = 0
     for i in range(0, len(data['data']), 1):
-        if i not in duplicate_list and int(data['data'][i]['Crash']) >= User_Input.Criteria_count:
+        if i not in duplicate_list and int(data['data'][i]['Crash'].replace('k', '000')) >= User_Input.Criteria_count:
             h_occurrences, h_crash_rate_percent, h_crash_rate = history_occurrences_catcher(data['data'][i]['RecentActivity'], crash_rate_data)
             h_slope = history_crash_rate_slope_calculator(h_crash_rate)
             print(h_slope)
@@ -368,7 +367,7 @@ def fabric_warning_handler(column_d_data, spreadsheet_id, service):
             start_column = 0
             end_column = 12
             sheet_id = User_Input.sheet_id_all
-            if int(crash_count[0]) >= 100:
+            if int(crash_count[0].replace('k', '000')) >= 100:
                 update_text_color = sheet_update_text_color(i+1, i+2, start_column, end_column, 1, 0, 0, sheet_id, spreadsheet_id, service)
                 print(update_text_color)
 
